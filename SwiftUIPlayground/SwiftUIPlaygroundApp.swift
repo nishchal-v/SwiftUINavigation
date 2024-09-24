dd849c29e3fbe53c15feadab9a67f195cb5075a2
Nishchal Visavadiya <nishchal.v@simformsolutions.com>
2024-09-24 17:44:04 +0530

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Nishchal Visavadiya on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
